f7975760dcef1fb5f175cf66ce9ab2db387ae066
Rob Arroyo <betoarpi@gmail.com>
2021-10-14 21:55:16 -0500

    :sparkles: Completed project with MVC Pattern

--- Destini-iOS13/Model/Story.swift
+++ Destini-iOS13/Model/Story.swift
@@ -7,3 +7,19 @@
 //
 
 import Foundation
+
+struct Story {
+    let titleText : String
+    let choice1Text :String
+    let choice1Value : Int
+    let choice2Text : String
+    let choice2Value : Int
+    
+    init(title: String, choice1: String, choice1Destination: Int, choice2: String, choice2Destination: Int) {
+        titleText = title
+        choice1Text = choice1
+        choice1Value = choice1Destination
+        choice2Text = choice2
+        choice2Value = choice2Destination
+    }
+}
